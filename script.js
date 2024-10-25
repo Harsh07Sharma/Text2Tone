5ca5eaa9cd270fcc5157b48a20e8e239bf96dde3
Harsh Sharma <121617099+Harsh07Sharma@users.noreply.github.com>
2024-10-25 21:06:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,21 +3,52 @@ let speech = new SpeechSynthesisUtterance();
 let voices = [];
 let voiceSelect = document.querySelector("select");
 
-window.speechSynthesis.onvoiceschanged = () => {
+
+function loadVoices() {
     voices = window.speechSynthesis.getVoices();
-    speech.voice = voices[0];
 
-    voices.forEach((voice,i) => (voiceSelect.options[i] =  new Option(voice.name,i)));
+   
+    console.log("Available voices:", voices);
+
+    
+    voiceSelect.innerHTML = "";
+
  
-};
+    let hindiVoices = voices.filter(voice => voice.lang.includes("hi"));
+    let otherVoices = voices.filter(voice => !voice.lang.includes("hi"));
 
-voiceSelect.addEventListener("change",() => {
-    speech.voice = voices[voiceSelect.value];
-})
+    
+    hindiVoices.forEach((voice, index) => {
+        let option = new Option(`${voice.name} (Hindi)`, `${index}`);
+        voiceSelect.appendChild(option);
+    });
+
+    
+    otherVoices.forEach((voice, index) => {
+        let option = new Option(`${voice.name} (${voice.lang})`, `${hindiVoices.length + index}`);
+        voiceSelect.appendChild(option);
+    });
+
+    
+    if (hindiVoices.length > 0) {
+        speech.voice = hindiVoices[0];
+    } else if (voices.length > 0) {
+        speech.voice = voices[0];
+    }
+}
 
 
+window.speechSynthesis.onvoiceschanged = () => loadVoices();
+setTimeout(loadVoices, 100); 
 
-document.querySelector("button").addEventListener("click",() => {
+
+voiceSelect.addEventListener("change", () => {
+    let selectedVoiceIndex = parseInt(voiceSelect.value);
+    speech.voice = voices[selectedVoiceIndex];
+});
+
+
+document.querySelector("button").addEventListener("click", () => {
     speech.text = document.querySelector("textarea").value;
     window.speechSynthesis.speak(speech);
-})
+});
